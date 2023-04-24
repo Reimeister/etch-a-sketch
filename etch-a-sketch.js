d3f4162c15b27d94c5c05f8339a915ebda9f0e07
Reimeister <128944245+Reimeister@users.noreply.github.com>
2023-04-24 18:46:24 -0500

    Add custom grid size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,7 @@
-let columns = 100;
-let rows = 100;
-let rowHeight = 600 / rows - 2; //formula to calculate the height of each individual row
+let columns = 16;
+let rows = 16; //formula to calculate the height of each individual row
 //flexbox creates base for columns and container, but JS is more reliable to create rows
-
+//2 represents 2px border, change this value if you decide to change the borders in CSS
 let container = document.createElement("div");
 container.className = "container";
 
@@ -11,10 +10,10 @@ function createGrid() {
     //loop for columns
     let column = document.createElement("div"); //create column
     column.className = "column";
-
     for (let j = 0; j < rows; ++j) {
       //loop for rows to complete grid
       let row = document.createElement("div");
+      let rowHeight = 600 / rows - 2;
       row.className = "row";
       row.style.height = `${rowHeight}px`; //dynamically changes height according to the dimensions of the grid
       row.addEventListener("mouseover", function (e) {
@@ -27,10 +26,24 @@ function createGrid() {
 }
 document.body.appendChild(container); //apply grid onto the body of the document
 //attach the 'hover' command to the ROWS, those are what creating the squares individually
-createGrid();
 
 function resetSquares() {
   let container = document.querySelector(".container");
   let squares = container.querySelectorAll("div");
   squares.forEach((div) => (div.style.backgroundColor = "#ffffff"));
 }
+
+function resizeGrid() {
+  let errorMessage = document.createElement("div");
+  let newValue = document.getElementById("gridsize").value;
+  if (newValue > 0 && newValue <= 100) {
+    document.querySelector(".container").textContent = "";
+    columns = newValue;
+    rows = newValue;
+    createGrid();
+  } else {
+    alert("Error! Please choose a number between 1 and 100");
+  }
+}
+
+createGrid();
