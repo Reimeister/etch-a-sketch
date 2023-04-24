52fe7459dbfb3a02d6ed431ba46f37b05507b400
Reimeister <128944245+Reimeister@users.noreply.github.com>
2023-04-24 04:39:17 -0500

    Add reset button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,21 +4,30 @@ let rows = 16;
 let container = document.createElement("div");
 container.className = "container";
 
-for (let i = 0; i < columns; ++i) {
-  //loop for columns
-  let column = document.createElement("div"); //create column
-  column.className = "column";
+function createGrid() {
+  for (let i = 0; i < columns; ++i) {
+    //loop for columns
+    let column = document.createElement("div"); //create column
+    column.className = "column";
 
-  for (let j = 0; j < rows; ++j) {
-    //loop for rows to complete grid
-    let row = document.createElement("div");
-    row.className = "row";
-    row.addEventListener("mouseover", function (e) {
-      row.style.backgroundColor = "black";
-    });
-    column.appendChild(row); //attach row to column
+    for (let j = 0; j < rows; ++j) {
+      //loop for rows to complete grid
+      let row = document.createElement("div");
+      row.className = "row";
+      row.addEventListener("mouseover", function (e) {
+        row.style.backgroundColor = "black";
+      });
+      column.appendChild(row); //attach row to column
+    }
+    container.appendChild(column); //put column in container, column already has the row
   }
-  container.appendChild(column); //put column in container, column already has the row
 }
 document.body.appendChild(container); //apply grid onto the body of the document
 //attach the 'hover' command to the ROWS, those are what creating the squares individually
+createGrid();
+
+function resetSquares() {
+  let container = document.querySelector(".container");
+  let squares = container.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "#ffffff"));
+}
