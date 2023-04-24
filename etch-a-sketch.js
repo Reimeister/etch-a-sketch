0218b2be2f0f71c9e75e4c2b51907c0b353f6dcf
Reimeister <128944245+Reimeister@users.noreply.github.com>
2023-04-24 03:44:36 -0500

    Implement mouseover effect on individual cells

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,20 +1,24 @@
-var columns = 16;
-var rows = 16;
+let columns = 16;
+let rows = 16;
 
-var container = document.createElement("div");
+let container = document.createElement("div");
 container.className = "container";
-for (var i = 0; i < columns; ++i) {
+
+for (let i = 0; i < columns; ++i) {
   //loop for columns
-  var column = document.createElement("div"); //create column
+  let column = document.createElement("div"); //create column
   column.className = "column";
 
-  for (var j = 0; j < rows; ++j) {
+  for (let j = 0; j < rows; ++j) {
     //loop for rows to complete grid
-    var row = document.createElement("div");
+    let row = document.createElement("div");
     row.className = "row";
-
+    row.addEventListener("mouseover", function (e) {
+      row.style.backgroundColor = "black";
+    });
     column.appendChild(row); //attach row to column
   }
   container.appendChild(column); //put column in container, column already has the row
 }
 document.body.appendChild(container); //apply grid onto the body of the document
+//attach the 'hover' command to the ROWS, those are what creating the squares individually
