5a4f6d83345e4c4f00dd0bfb4ab153bac95ab76e
Reimeister <128944245+Reimeister@users.noreply.github.com>
2023-04-23 20:54:21 -0500

    Code JS to produce 16x16 grid

---
+++ etch-a-sketch.js
@@ -0,0 +1,20 @@
+var columns = 16;
+var rows = 16;
+
+var container = document.createElement("div");
+container.className = "container";
+for (var i = 0; i < columns; ++i) {
+  //loop for columns
+  var column = document.createElement("div"); //create column
+  column.className = "column";
+  column.textContent = "Hello Mom";
+  for (var j = 0; j < rows; ++j) {
+    //loop for rows to complete grid
+    var row = document.createElement("div");
+    row.className = "row";
+    row.textContent = "Hi Mom";
+    column.appendChild(row); //attach row to column
+  }
+  container.appendChild(column); //put column in container, column already has the row
+}
+document.body.appendChild(container); //apply grid onto the body of the document
